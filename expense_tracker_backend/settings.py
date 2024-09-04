a4373e0c7b695874053557bb6ed1342cad4abdfa
ellyjj1 <72015673+ellyjj1@users.noreply.github.com>
2024-09-05 09:58:38 +1200

    simplified the configuration of vercel

--- expense_tracker_backend/settings.py
+++ expense_tracker_backend/settings.py
@@ -71,22 +71,13 @@ else:
 
 IS_VERCEL = os.environ.get('VERCEL', False)
 
-if IS_VERCEL:
-    # Vercel environment (use PostgreSQL)
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=os.environ.get('DATABASE_URL'),
-            conn_max_age=600
-        )
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    # Local environment (use SQLite)
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
     }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
