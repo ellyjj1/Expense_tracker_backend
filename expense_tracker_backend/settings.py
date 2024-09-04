00478223d246e1c61e644c064426a090ef96376b
ellyjj1 <72015673+ellyjj1@users.noreply.github.com>
2024-09-04 17:45:49 +1200

    make  the backend can CD on vercel

--- expense_tracker_backend/settings.py
+++ expense_tracker_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pxma$-v^oy8he6*e#y-1d(2rqlg-1!y(&9@yz39%4u*olq()d+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -70,8 +70,8 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'expense_tracker_backend.wsgi.application'
-
+# WSGI_APPLICATION = 'expense_tracker_backend.wsgi.application'
+WSGI_APPLICATION = 'api.wsgi.app'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
