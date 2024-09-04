efc9750b735c7ea61dac5bbbbc574d16f0934247
ellyjj1 <72015673+ellyjj1@users.noreply.github.com>
2024-09-04 18:40:12 +1200

    update vercel config -v2

--- expense_tracker_backend/wsgi.py
+++ expense_tracker_backend/wsgi.py
@@ -10,8 +10,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expense_tracker_backend.settings')
 
-# application = get_wsgi_application()
-app = get_wsgi_application()
+application = get_wsgi_application()
+app = application
